feb7d41e7b27efde53bd1e5cfad3ec27b88a1086
Ryder Baez <rbbaez@usc.edu>
2023-10-03 19:17:38 -0700

    spotify remove parameter

--- my_spotify.py
+++ my_spotify.py
@@ -7,6 +7,40 @@ import random
 # Spotify API: https://api.spotify.com
 # I ended up using only post_auth() and spotify_init() because the other authetication methods required more overhead
 
+def post_auth():
+    #This function uses the Client Credentials Flow on Spotify API which allows you
+    #to access publicly available information, but not user specific stuff (which is 
+    #a shame). I'm using it here because I don't want to deploy a localhost.
+
+    #spotify api requires you to create an app on spotify developer and send a post request
+    #using the client id and client secret that is provided to you.
+    client_id = 'e7efe30cb6ab46c8a85bed9ff5ef5cae'
+    client_secret = 'df75e915e70d4863aa852d4c92be7b54'
+    credentials = f"{client_id}:{client_secret}"
+
+    data = {
+        'grant_type': "client_credentials",
+    }
+
+    headers = {
+        #The Authorization key format that Spotify requires is the base64 encoding of the credentials.
+        #I used fstrings here but there are other ways to do this as well
+        'Authorization': f"Basic {base64.b64encode(credentials.encode()).decode()}",
+        'Content-Type': "application/x-www-form-urlencoded"
+    }
+    postreq = requests.post("https://accounts.spotify.com/api/token",
+                            data=data, headers=headers)
+
+    response_data = postreq.json()
+
+    #get access token information (expires in 3600s/1hr)
+    # print(json.dumps(response_data, indent=4))
+
+    access_token = response_data["access_token"]
+    token_type = response_data["token_type"]
+
+    return (token_type, access_token)
+
 def get_auth():
     # This function uses the default authentication method that the Spotify API uses
     # for web applications. It needs a functional redirect_uri so that we can retrieve
@@ -64,40 +98,6 @@ def get_userID(auth_string):
 
 def spotify_init():
 
-    def post_auth():
-        #This function uses the Client Credentials Flow on Spotify API which allows you
-        #to access publicly available information, but not user specific stuff (which is 
-        #a shame). I'm using it here because I don't want to deploy a localhost.
-
-        #spotify api requires you to create an app on spotify developer and send a post request
-        #using the client id and client secret that is provided to you.
-        client_id = 'e7efe30cb6ab46c8a85bed9ff5ef5cae'
-        client_secret = 'df75e915e70d4863aa852d4c92be7b54'
-        credentials = f"{client_id}:{client_secret}"
-
-        data = {
-            'grant_type': "client_credentials",
-        }
-
-        headers = {
-            #The Authorization key format that Spotify requires is the base64 encoding of the credentials.
-            #I used fstrings here but there are other ways to do this as well
-            'Authorization': f"Basic {base64.b64encode(credentials.encode()).decode()}",
-            'Content-Type': "application/x-www-form-urlencoded"
-        }
-        postreq = requests.post("https://accounts.spotify.com/api/token",
-                                data=data, headers=headers)
-
-        response_data = postreq.json()
-
-        #get access token information (expires in 3600s/1hr)
-        # print(json.dumps(response_data, indent=4))
-
-        access_token = response_data["access_token"]
-        token_type = response_data["token_type"]
-
-        return (token_type, access_token)
-
     auth_string = post_auth()
 
     headers = {
@@ -145,8 +145,11 @@ SPOTIFY_APP = {
 
 if __name__ == '__main__':
     # retrieve_post = post_auth()
+
+    # auth_string = retrieve_post[0] + " " + retrieve_post[1]
+
     spotify_output = spotify_init()
-    
+
     song_text = "song: " + spotify_output[0] + ", artists: "
 
     for i in range(0, len(spotify_output[1])):
